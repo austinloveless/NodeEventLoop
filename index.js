db058e44c6b2b154df0727434cd605574dbfdfeb
austinloveless <austinloveless5171@gmail.com>
2019-01-12 15:10:03 -0700

    index.js file shows an example of clustering

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-const express = require("express");
-const app = express();
+process.env.UV_THREADPOOL_SIZE = 1;
+const cluster = require("cluster");
 
-app.get("/", (req, res) => {
-  res.send("hi there");
-});
+//Is the file being executed in master mode?
+if (cluster.isMaster) {
+  //Cause index.js to be executed *again* but
+  //in child mode
+  cluster.fork();
+  cluster.fork();
+} else {
+  // Im a child, Im going to act like a server
+  // and do nothing else
+  const express = require("express");
+  const crypto = require("crypto");
+  const app = express();
 
-app.listen(3000);
+  app.get("/", (req, res) => {
+    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+      res.send("hi there");
+    });
+  });
+
+  app.get("/fast", (req, res) => {
+    res.send("this was fast");
+  });
+
+  app.listen(3000);
+}
